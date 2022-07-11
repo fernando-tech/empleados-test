a4d791dab869205b741ee928a5fac21ab0ac3038
Fernando Lugo <fernandolugo@aragon.unam.mx>
2022-07-11 00:50:06 -0500

    se importa routerModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -20,7 +21,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
